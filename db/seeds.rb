ae42350787e60f8f50435f88ead237833a3650fa
Mike Schechter <schechtermike@gmail.com>
2013-06-14 10:02:21 -0400

    fixes validations issues with seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,17 @@ User.delete_all
 Rating.delete_all
 Hotspot.delete_all
 
-[['Brian', '246 Mott st. Ny, Ny,'],
- ['General Assembly', '10 E 21st st. Ny, Ny,'],
- ['george', '97 Arden st, Ny, Ny,'],
- ['mike', "533 9th ave. Ny, Ny."],
- ['remy', '20 exchange, 10005'],
- ['lee', '533 9th ave. ny. ny.']].map do |user, location|
-	User.create(name: user, location: location, password: 'foobar')
+[['Brian', '246 Mott st. Ny, Ny,', 'abc@example.com'],
+ ['General Assembly', '10 E 21st st. Ny, Ny,', 'abcd@example.com'],
+ ['george', '97 Arden st, Ny, Ny,', 'abce@example.com'],
+ ['mike', "533 9th ave. Ny, Ny.", 'abcf@example.com'],
+ ['remy', '20 exchange, 10005', 'abcg@example.com'],
+ ['lee', '533 9th ave. ny. ny.', 'abch@example.com']].map do |user, location, email|
+	User.create(name: user, location: location, password: 'foobar', email: email)
 end
 
 
-User.create(email: 'foo@example.com', password: 'bar', admin: true)
+u = User.new(name: 'foobar', location: '800 mott st, ny, ny', password: 'foobar', email: 'foo@example.com')
+
+u.admin = true
+u.save!
